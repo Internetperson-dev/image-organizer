1bfc751bdc90f4ef0846fb0591b70fe986e29803
Ram10 <Rameezmajid10@outlook.com>
2026-01-26 18:45:26 +0000

    Update esbuild.config.mjs

--- src/esbuild.config.mjs
+++ src/esbuild.config.mjs
@@ -1,12 +1,10 @@
 import esbuild from "esbuild";
 
 await esbuild.build({
-  entryPoints: ["src/main.ts"],
+  entryPoints: ["main.ts"],
   bundle: true,
   external: ["obsidian"],
   format: "cjs",
   target: "es2018",
   outfile: "main.js"
 });
-
-This produces the single file O
